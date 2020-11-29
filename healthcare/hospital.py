334c4977adb72546347df5399af498d52ec29822
aarav <josanaarav@gmail.com>
2020-11-29 20:39:58 +0530

    made a lot of changes

--- healthcare/hospital.py
+++ healthcare/hospital.py
@@ -1,29 +1,52 @@
 import json
+import os
 
 class hospital:
-    worker_dict = {}
     current_largest_id = 10000
+
+    def __init__(self):
+        self.worker_dict = {
+            "doctor" :{"cardiologist" : {"busy" : {}, "free" : {}},
+            "audiologist" : {"busy" : {}, "free" : {}},
+            "paediatrician" : {"busy" : {}, "free" : {}}, 
+            "veterinarian" : {"busy" : {}, "free" : {}},
+            "radiologist" : {"busy" : {}, "free" : {}}},
+            "nurse" : {"busy" : {}, "free" : {}},
+            "other-health-worker-type" : {"busy" : {}, "free" : {}},
+            "largest_id" : "\"\"" 
+            }
+
+    def generate_unique_worker_id(self):
+        self.current_largest_id += 1
+        self.worker_dict["largest_id"] = self.current_largest_id
+        return self.current_largest_id
 
     def update_worker_DB(self):
         with open('healthcare/worker_db.json', 'w') as f:
             json.dump(self.worker_dict, f, indent=4)
 
     def read_worker_DB(self):
+        if os.stat("healthcare/worker_db.json").st_size == 0:
+            return
         with open('healthcare/worker_db.json') as f:
             self.worker_dict = json.load(f)
-        self.current_largest_id = int(max(self.worker_dict.keys()))
-
-    def generate_unique_worker_id(self):
-        self.current_largest_id += 1
-        return self.current_largest_id
-
+        self.current_largest_id = self.worker_dict["largest_id"]
+            
+         
     def add_healthworker(self):
         name = input("Health worker name: ")
-        job_title = input("Health worker job title:")
+        job_title = input("Health worker job title: ")
+        worker_type = input("Enter worker type: ").lower().strip()
         id = self.generate_unique_worker_id()
-        self.worker_dict[id] = { "name" : name, "job_title" : job_title , "busy" : False}
+        if job_title in self.worker_dict["doctor"]:
+            if worker_type == "doctor":
+                self.worker_dict["doctor"][job_title]["free"][id] = { "name" : name}
+        elif worker_type == "nurse": 
+            self.worker_dict["nurse"]["free"][id] = { "name" : name, "job_title" : job_title} 
+        else:
+            self.worker_dict["other-health-worker-type"]["free"][id] = { "name" : name, "job_title" : job_title} 
         self.update_worker_DB()
-        
+
     def remove_healthworker(self):
         id = input("Enter Health worker ID: ")
         self.worker_dict.pop(id)
@@ -32,29 +55,22 @@ class hospital:
     def print_worker_dict(self):
         print(self.worker_dict)
 
-    def get_doctor(self):
-        bFound = False
-        doctor_type = input("Enter doctor type ").strip()
-        for i in self.worker_dict:
-            a = self.worker_dict[i]["job_title"]
-            if a == doctor_type:
-                print("Doctor name:" + self.worker_dict[i]["name"] + " doctor busy: " 
-                + str(self.worker_dict[i]["busy"]))
-                bFound = True   
+    def book_doctor(self):
+        doctor_type = input("Enter doctor type ").strip().lower()
+        if doctor_type in self.worker_dict["doctor"]:
+            a = self.worker_dict["doctor"][doctor_type]["free"]
+            self.worker_dict["doctor"][doctor_type]["busy"] = a
+            for i in (self.worker_dict["doctor"][doctor_type]["free"]):
+                del self.worker_dict["doctor"][doctor_type]["free"][i]
                 break
-        if bFound == False:
-            print("Enter a valid doctor type")
-        
-    def set_busy_status(self):
-        doctor_id = input("Enter a doctors id: ")
-        busy_status = input("Enter doctor status (busy or free) : ").lower().strip()   
-        if busy_status == "busy":
-            self.worker_dict[doctor_id]["busy"] = True
-        elif busy_status == "free":
-            self.worker_dict[doctor_id]["busy"] = False
-        else:
-            print("Enter valid status")
         self.update_worker_DB()
+                 
+
+        
+    def set_doctor_free(self):
+        doctor_id = int(input("Enter a doctors id: "))
+        print(self.worker_dict)
+        # radiologist
     
 
             
