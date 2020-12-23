0ff2a1868f1e5d477dd3ecfe0de4c47ed76bacf4
Aarav Josan <josanaarav@gmail.com>
2020-12-23 10:17:54 +0530

    Final version

--- priority_queue/priorityqueue.py
+++ priority_queue/priorityqueue.py
@@ -1,84 +1,50 @@
-import time
+dict = {}
+i = 0
 
-class Node:
-    def __init__(self, value, next_node = None):
-        self.value = value
-        self.next_node = next_node
-    
-    def get_value(self):
-        return self.value
-    
-    def get_next_node(self):
-        return self.next_node
-    
-    def set_next_node(self, next_node):
-        self.next_node = next_node
-    
-class Queue:
-    def __init__(self, a):
-        self.head = None
-        self.tail = None
-        self.max_size = 3
-        self.total_prioirity_messages = a
-        self.current_prioirity_messages = 0
-        self.current_size = 0
-    
-    def prioirity_has_space(self):
-        return self.total_prioirity_messages > self.current_prioirity_messages
+while i != 1:
+    try:
+        number_of_priorities = int(input("Enter number of priorities: "))
+    except ValueError:
+        continue
+    i += 1
 
-    def has_space(self):
-        return self.max_size > self.current_size
+for i in range(number_of_priorities):
+    dict[i + 1] = []
 
-    def add(self, value):
-        if self.has_space():
-            if self.tail is None:
-                self.head = Node(value)
-                self.tail = self.head
-                self.current_size += 1
-            else:
-                self.tail.next_node = Node(value)
-                self.tail = self.tail.next_node
-                self.current_size += 1
 
-    def remove(self):
-        if self.head is None:
-            return None
+def print_dict():
+    for i in (dict.keys()):
+        print((i, dict[i]), end = " " + "\n")
+
+def pop_top():
+    for i in (dict.keys()):
+        if dict[i] == []:
+            continue
         else:
-            to_return = self.head.value
-            self.head = self.head.next_node
-            return to_return + " was sent."
-            self.current_size -= 1
+            top_message = dict[i][0]
+            break
+    dict[i].remove(top_message)
+    return top_message
 
-prioirity = []
-not_prioirity = []
-
-number = int(input("Enter number of prioirity messages: "))
-a = Queue(number)
-
-def message_input():
-    while a.has_space():
-        message = input("Enter your message: ")
-        yes_or_no = input("Is the message important(y/n): ").lower().strip()
-        if yes_or_no == "y":
-            if a.prioirity_has_space():
-                prioirity.append(message)
-                a.current_prioirity_messages += 1
-                a.current_size += 1
-            else:
-                print("No more space in priority message")
-        elif yes_or_no == "n":
-            not_prioirity.append(message)
-            a.current_size += 1
+def main():
+    while True:
+        try:
+            number = int(input("Enter the priority of new message or press 0 to pop, -1 to print all: "))
+        except ValueError:
+            continue
+        if number == 0:
+            print("Top message is: " + pop_top())
+            continue
+        elif number == -1:
+            print_dict()
+            continue
+        if number in dict:
+            message = input("Enter message: ")
+            try:
+                dict[key] = list() if (number not in dict) else dict[number].append(message)
+            except NameError:
+                continue
         else:
-            message_input()
-    a.current_size = 0
+            print("No priority found")
 
-message_input()      
-final_list = prioirity + not_prioirity
-
-for message in final_list:
-    a.add(message)
-
-for i in range(a.current_size):
-    time.sleep(3)
-    print(a.remove())
+main()
